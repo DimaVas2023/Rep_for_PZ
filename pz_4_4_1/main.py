7480f20056cd472da7ec01d9aa5be31c54cf94b0
DimaVas2023 <Dima.rksi.vas@gmail.com>
2023-10-12 01:22:26 +0300

    pz 3

--- pz_4_4_1/main.py
+++ pz_4_4_1/main.py
@@ -4,21 +4,3 @@
 # for i in range(1, 11):
 #     print(f' стоимость {i/10} кг конфет - {konfeta * i / 10} рублей')
 
-
-n = int(input())
-print(n%10)
-
-n = int(input())
-print(n%10,n//10)
-
-n = int(input())
-print(n%10,(n//10)%10,n//100)
-
-n = int(input())
-print(n%10,(n//10)%10,(n//100)%10, n//1000)
-#
-n = int(input())
-print(n%10,(n//10)%10,(n//100)%10,(n//1000)%10,n//10000)
-
-n = int(input())
-print(n%10,(n//10)%10,(n//100)%10,(n//1000)%10,(n//10000)%10,n//100000)
