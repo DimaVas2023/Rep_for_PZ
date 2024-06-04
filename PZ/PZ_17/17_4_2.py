d818d98c7a7431532285f2d65238eaf3e3085b0f
CaptainSliva <cripton2020@mail.ru>
2024-06-04 19:23:05 +0300

    update PZ 17

--- PZ/PZ_17/17_4_2.py
+++ PZ/PZ_17/17_4_2.py
@@ -1,39 +1,27 @@
-# Задание предполагает, что у студента есть проект с практическими работами (NoNo 2-13),
-# оформленный согласно требованиям. Все задания выполняются c использованием модуля
-# OS
+# Разработать программу с применением пакета tk, взяв в качестве условия одну
+# любую задачу из ПЗ NoNo 2 – 9.
 
 
-import os
+import tkinter as tk
 
+def to_meters():
+    l = float(entry_l.get())
+    meters = l / 100
+    whole_meters = int(meters)
+    meters_result.config(text=f"Количество полных метров: {whole_meters}.")
 
-def size(pathf):
-    for i in os.listdir(pathf):
-        if os.path.isfile(i):
-            os.path.getsize(i)
+root = tk.Tk()
+root.title("Сантиметры в метры")
 
+label_l = tk.Label(root, text="Длина в сантиметрах:")
+label_l.pack()
+entry_l = tk.Entry(root)
+entry_l.pack()
 
-pz_11 = os.listdir('PZ_11')
-print(pz_11)
+button_calculate = tk.Button(root, text="Посчитать", command=to_meters)
+button_calculate.pack()
 
-os.makedirs('../test/test1')
+meters_result = tk.Label(root, text="")
+meters_result.pack()
 
-os.replace('6_4_1.py', '/home/student/Документы/PycharmProject/IS-27/Proj 1sem Vasilchenko/Rep_for_PZ/test/6_4_1.py')
-os.replace('6_4_2.py', '/home/student/Документы/PycharmProject/IS-27/Proj 1sem Vasilchenko/Rep_for_PZ/test/6_4_2.py')
-os.replace('7_4_1.py', '/home/student/Документы/PycharmProject/IS-27/Proj 1sem Vasilchenko/Rep_for_PZ/test/test1/7_4_1.py')
-os.rename('/home/student/Документы/PycharmProject/IS-27/Proj 1sem Vasilchenko/Rep_for_PZ/test/test1/7_4_1.py', '/home/student/Документы/PycharmProject/IS-27/Proj 1sem Vasilchenko/Rep_for_PZ/test/test1/test.txt')
-
-for i in os.listdir():
-    if os.path.isfile(i):
-        os.path.getsize(i)
-
-size('../test')
-size('../test/test1')
-
-short_file = '11111111111111111111111111'
-for i in pz_11:
-    if len(i) < len(short_file):
-        short_file = i
-print(short_file)
-
-os.remove('/home/student/Документы/PycharmProject/IS-27/Proj 1sem Vasilchenko/Rep_for_PZ/test/test1/test.txt')
-
+root.mainloop()
